482f38335834e96a300c116dd1c65879e9ebd549
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-10 18:05:03 -0300

    Figma For Business

---
+++ FigmaForBusiness/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Clash': ['Clash Display'],
+        'Satoshi': ['Satoshi']
+      },
+      colors:{
+        'Primary':['#380099'],
+        'Amber':['#FFBB00'],
+        'Folly':['#FF0055'],
+      }
+    },
+  },
+  plugins: [],
+}
+
