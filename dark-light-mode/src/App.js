b7a061f6fc865d9ecbc7e78ae66ae24b6e348aaf
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 11:46:24 -0300

    build function, toogle and setting dark class

--- dark-light-mode/src/App.js
+++ dark-light-mode/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  //setting 
+  function toogleDark(){
+    document.documentElement.classList.toggle("dark")
+  }
+
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
+    <div className="w-full h-screen flex flex-col items-center justify-center dark:bg-yellow-400">
+      <button className='p-2 bg-black text-white font-bold w-[200px] rounded-md absolute right-10 top-10 dark:bg-purple-700 dark:text-white'
+      onClick={toogleDark}>
+        Dark Mode
+      </button>
       <div className=' w-full max-w-5xl flex flex-col gap-4 items-center justify-center'>
-      <h1 className='font-bold text-center text-4xl w-1/2'>It's crazy how Blues Music can Connect Souls</h1>
-      <p className='text-3xl text-center w-1/2'>Sometimes music is the only thing that gets your mind off of everything else.</p>
-      <button className='p-2 bg-black text-white w-[200px] rounded-md'>
+      <h1 className='font-bold text-center text-4xl w-1/2 dark:text-purple-700'>It's crazy how Blues Music can Connect Souls</h1>
+      <p className='text-3xl text-center w-1/2 dark:text-purple-700'>Sometimes music is the only thing that gets your mind off of everything else.</p>
+      <button className='p-2 bg-black text-white font-bold w-[200px] rounded-md dark:bg-purple-700 dark:text-white dark:hover:scale-105'>
         Play some music
       </button>
     </div>
--- dark-light-mode/tailwind.config.js
+++ dark-light-mode/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  
+  //Set Darkmode - class
+  darkMode:'class',
+  
   theme: {
     extend: {},
   },
