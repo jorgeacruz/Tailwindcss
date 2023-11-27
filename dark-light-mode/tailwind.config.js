259199e5ad1f66cf3fc41094bfadbbffe3913391
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 13:59:59 -0300

    Setting darkMode:media, change using OS user

--- dark-light-mode/tailwind.config.js
+++ dark-light-mode/tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  //if you need the USER CHANGE THEME
+  // set this.. 
   darkMode:'class',
+
+  //but if you need the theme CHANGE USING OS user THEME (light - dark) 
+  //set this (darMode:'media')
+  
   theme: {
     extend: {},
   },
