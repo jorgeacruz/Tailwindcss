792b5fd4dcc757e3d1a2be0f0a6cb7545d2420f3
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 13:00:40 -0300

    correting erros

---
+++ dark-light-mode/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  
+  //if you wanna the USER CHANGE Theme,
+  //set this:
+  darkMode:'class',
+
+
+  //if you wanna the OS CHANGE Theme,
+  //set this:
+  //darkMode:'media',
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
