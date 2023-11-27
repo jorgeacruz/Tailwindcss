bdd5bb01efaf9a4051f85713686a70773fe5047c
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 10:48:34 -0300

    Initial Setting

--- dark-light-mode/src/App.js
+++ dark-light-mode/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <div className=' w-full max-w-5xl flex flex-col gap-4 items-center justify-center'>
+      <h1 className='font-bold text-center text-4xl w-1/2'>It's crazy how Blues Music can Connect Souls</h1>
+      <p className='text-3xl text-center w-1/2'>Sometimes music is the only thing that gets your mind off of everything else.</p>
+      <button className='p-2 bg-black text-white w-[200px] rounded-md'>
+        Play some music
+      </button>
+    </div>
+
     </div>
   );
 }
