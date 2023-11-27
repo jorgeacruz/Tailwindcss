af9e5cd8d4bc0357a3f6327c03d5100e7bbd3f28
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 12:23:05 -0300

    Dark - Light Mode

--- dark-light-mode/src/App.js
+++ dark-light-mode/src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 
 function App() {
 
-  //setting 
+  function ThemeD() {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = 'light'
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = 'dark'
+
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem('theme')
+  }
+
+  //setting dark class
   function toogleDark(){
     document.documentElement.classList.toggle("dark")
   }
