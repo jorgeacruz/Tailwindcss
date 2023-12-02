48db922e9dda734523bdfcce20301c2f27e272f2
Jorge Cruz <j.alexcruz@gmail.com>
2023-12-02 11:02:52 -0300

    WebFood - Project

---
+++ web-food/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+     <h1 className='text-black text-5xl font-bold'>Começando um Projeto Tailwind + React</h1>
+    </div>
+  );
+}
+
+export default App;
