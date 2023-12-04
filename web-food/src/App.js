7e25044d0464265b4b41600ebe9176382ed8ffef
Jorge Cruz <j.alexcruz@gmail.com>
2023-12-04 17:49:16 -0300

    Building mobile menu

--- web-food/src/App.js
+++ web-food/src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-gray-300">
       <NavBar/>
     </div>
   );
--- web-food/src/components/NavBar.js
+++ web-food/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai'
 import { BsFillCartFill } from 'react-icons/bs'
+import { TbTruckDelivery } from 'react-icons/tb'
 
 export default function NavBar() {
  return (
@@ -27,6 +28,20 @@ export default function NavBar() {
         <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
             <BsFillCartFill size={25} className="mr-2"/> Cart
         </button>
+
+        {/** mobile menu */}
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 letf-0">
+        </div>
+        {/** side drawer menu */}
+        <div className="fixed top-0 left-0 w-[300px] h-screen z-10 bg-white duration-300">
+           <AiOutlineClose size={30} className="absolute right-4 top-4 cursor-pointer" />
+           <h2 className="text-2xl p-4">Best <span className="font-bold">Eat</span></h2>
+           <nav>
+            <ul className="text-black">
+                <li><TbTruckDelivery/>Orders</li>
+            </ul>
+           </nav>
+        </div>
     </div>
   );
 }
