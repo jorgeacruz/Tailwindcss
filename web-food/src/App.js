1dcf70a265b3762dda06b7df40ef41d1b39c11e8
Jorge Cruz <j.alexcruz@gmail.com>
2023-12-19 19:34:24 -0300

    Add DarkMode

--- web-food/src/App.js
+++ web-food/src/App.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-//International
-import { useTranslation } from 'react-i18next';
-import './locales/i18n'
-
 // all components
 import NavBar from './components/NavBar';
 import Hero from './components/hero';
@@ -16,17 +12,6 @@ import Copyright from './components/copyright';
 
 function App() {
  
-  //i18n
-  const { t, i18n:{ changeLanguage, language }} = useTranslation()
-  const [currentLanguage, setCurrentLanguage] = useState(language);
-  
-  function handleChangeLanguage(){
-   
-    const newLanguage  = currentLanguage === 'pt' ? 'en' : 'pt'
-    changeLanguage(newLanguage)
-    setCurrentLanguage(newLanguage)
-
-  }
   return (
     <div className="bg-black pb-3 dark:bg-white">
       <NavBar/>
