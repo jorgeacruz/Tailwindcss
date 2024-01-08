96d15009aaddb212ff613e892cdfa8cfebc16ae5
Jorge Cruz <j.alexcruz@gmail.com>
2024-01-08 19:10:50 -0300

    updated files

--- Social-Links/tailwind.config.js
+++ Social-Links/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Hind_mid:'Hind Siliguri',
+      }
+    },
   },
   plugins: [],
 }
